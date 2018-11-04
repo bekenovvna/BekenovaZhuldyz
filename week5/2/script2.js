acc481d29e3c69bca2ffcac5a7e40e830623817f
bekenovvna <43030082+bekenovvna@users.noreply.github.com>
2018-11-04 18:21:53 +0600

    Add files via upload

---
+++ week5/2/script2.js
@@ -0,0 +1,7 @@
+function h(e){
+	 var imgReplace =document.getElementsByTagName("img")[0].src = e.target.src;
+}
+
+
+document.body.addEventListener('click', h);
+
---
+++ week5/script5.2.js
@@ -0,0 +1,40 @@
+alert(" Welcome !!!Are you sure that you want to see it? ")
+/*
+function onClick()
+{console.log("hello")
+
+}
+
+const button = document.querySelector("#carousel");
+button.addEventListener("click",onClick);*/
+/*
+function sayHello(){
+    document.querySelector("img").src="https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/At%20a%20glance/m-sportpackage-slide-01.jpg/_jcr_content/renditions/cq5dam.resized.img.485.low.time1489075534663.jpg";
+}
+function sayBye(){
+    document.querySelector("img").src="https://s.auto.drom.ru/i24207/c/photos/fullsize/daewoo/nexia/daewoo_nexia_695410.jpg";
+}
+document.querySelector("#carousel img").addEventListener('click',sayHello);
+document.querySelector("#carousel img").addEventListener('mouseleave',sayBye);*/
+
+function image1() {
+    document.querySelector("img").src="https://www.bmw.ua/content/dam/bmw/common/all-models/x-series/x6/2014/At%20a%20glance/m-sportpackage-slide-01.jpg/_jcr_content/renditions/cq5dam.resized.img.485.low.time1489075534663.jpg";
+    console.log("Changed to 1!")}
+
+function image2() {
+    document.querySelector("img").src="https://www.cstatic-images.com/car-pictures/xl/USC60AUC021B021001.png";
+    console.log("Changed to 2!")}  
+    
+function image3() {
+    document.querySelector("img").src="https://www.mercedes-benz.ru/content/media_library/hq/hq_mpc_reference_site/passenger_cars_ng/new_cars/models/gle-class/c292/modeloverview20/271x124/amgmodel_overview_mercedes-benz-gle-c292_mercedes-amg-gle63s4matic_271x124_11-2015_jpg.object-Single-MEDIA.tmp/amgmodel_overview_mercedes-benz-gle-c292_mercedes-amg-gle63s4matic_271x124_11-2015.jpg";
+    console.log("Changed to 3!")}
+
+function image4() {
+    document.querySelector("img").src="https://s.auto.drom.ru/i24207/c/photos/fullsize/daewoo/nexia/daewoo_nexia_695410.jpg";
+    console.log("Changed to 4!")}  
+    
+function image5() {
+    document.querySelector("img").src="http://st.motortrend.com/uploads/sites/10/2015/09/2015-Volkswagen-Touareg-front-view.jpg";
+    console.log("Changed to 5!")}
+
+    
